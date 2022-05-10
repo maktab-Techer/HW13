40e494d91f55a097fc2e6b70f74f6bf3d38a144f
Amirhossin Dehghani <103383658+MonsterDeh@users.noreply.github.com>
2022-05-10 22:26:37 +0430

    add Request

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -1,5 +1,19 @@
 <?php
 namespace App\Core ;
 class Request {
+    public function getPath()
+    {
+        $path=$_SERVER["REQUEST_URI"];
+        $index=strpos($path,"?");
+        return ($index==false) ?
+         $path :
+         substr($path ,0,$index);
+    }
+    public function getMethod()
+    {
+       return $_SERVER["REQUEST_METHOD"];
+    }
 
 }
+
+// var_dump($_SERVER);
