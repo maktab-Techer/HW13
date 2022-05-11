b0345fe40493ab3522aa96179d951b0f750278af
Amirhossin Dehghani <103383658+MonsterDeh@users.noreply.github.com>
2022-05-11 23:27:24 +0430

    remove css 404

--- View/Main.php
+++ View/Main.php
@@ -6,16 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <!-- 404 -->
-  <link href="css/style.css" rel="stylesheet">
+  
   <!-- Font Awesome -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
   <!-- MDB -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css" rel="stylesheet" />
-
-
 
 </head>
 
