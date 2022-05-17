27c5dd99481e9d5b0c1bad8f0f8d89f8327a0808
MonsterDeh <dehghani.sad.1@gmail.com>
2022-05-17 18:05:52 +0430

    add

--- View/Main.php
+++ View/Main.php
@@ -1,24 +1,140 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
+<!--   [XR&CO'2014], Tue, 22 Oct  -->
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  
-  <!-- Font Awesome -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-  <!-- MDB -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css" rel="stylesheet" />
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home 1</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+    <link rel="stylesheet" href="plugin/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="plugin/bootstrap/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="fonts/poppins/poppins.css">
+    <link rel="stylesheet" href="plugin/fonts/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="plugin/jquery-ui/jquery-ui.min.css">
+    <link rel="stylesheet" href="plugin/process-bar/tox-progress.css">
+    <link rel="stylesheet" href="plugin/owl-carouse/owl.carousel.min.css">
+    <link rel="stylesheet" href="plugin/owl-carouse/owl.theme.default.min.css">
+    <link rel="stylesheet" href="plugin/animsition/css/animate.css">
+    <link rel="stylesheet" href="plugin/jquery-ui/jquery-ui.min.css">
+    <link rel="stylesheet" href="plugin/mediaelement/mediaelementplayer.css">
+    <link rel="stylesheet" href="plugin/datetimepicker/bootstrap-datepicker3.css">
+    <link rel="stylesheet" href="plugin/datetimepicker/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="plugin/lightgallery/lightgallery.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
+  <!--load page-->
+<div class="load-page">
 
-  {{Content}}
+    <div class="sk-wave">
+        <div class="sk-rect sk-rect1"></div>
+        <div class="sk-rect sk-rect2"></div>
+        <div class="sk-rect sk-rect3"></div>
+        <div class="sk-rect sk-rect4"></div>
+        <div class="sk-rect sk-rect5"></div>
+    </div>
+</div>
+<!-- Mobile nav -->
+<nav class="visible-sm visible-xs mobile-menu-container mobile-nav">
+    <div class="menu-mobile-nav navbar-toggle">
+        <span class="icon-bar"><i class="fa fa-bars" aria-hidden="true"></i></span>
+    </div>
+    <div id="cssmenu" class="animated">
+        <div class="uni-icons-close"><i class="fa fa-times" aria-hidden="true"></i></div>
+        <ul class="nav navbar-nav animated">
+            <li class="has-sub"><a href="#">Home</a>
+                <ul>
+                    <li><a href="index-2.html">Home 1</a></li>
+                    <li><a href="01_02_home_2.html">Home 2</a></li>
+                    <li><a href="01_03_home_3.html">Home 3</a></li>
+                </ul>
+            </li>
+            <li class="has-sub"><a href='#'>Page</a>
+                <ul>
+                    <li><a href="02_01_about.html">About</a></li>
+                    <li class="has-sub"><a href="#">doctor</a>
+                        <ul>
+                            <li><a href="02_02_doctor.html">Doctors </a></li>
+                            <li><a href="02_03_doctor_details.html">Doctors Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="02_04_contact.html">Contact</a></li>
+                    <li><a href="02_05_gallery.html">Gallery</a></li>
+                </ul>
+            </li>
+            <li><a href="03_01_services.html">Services</a></li>
+            <li class="has-sub"><a href="#">Departments</a>
+                <ul>
+                    <li><a href="04_01_departments.html">Departments</a></li>
+                    <li><a href="04_02_single_department.html">Single Departments</a></li>
+                </ul>
+            </li>
+            <li class="has-sub"><a href='#'>Blog</a>
+                <ul>
+                    <li><a href="05_01_blog_list.html">Blog list</a></li>
+                    <li><a href="05_02_blog_grid.html">Blog Grid</a></li>
+                    <li><a href="05_03_single_post.html">Single post</a></li>
+                </ul>
+            </li>
+            <li class="has-sub"><a href='#'>shop</a>
+                <ul>
+                    <li><a href="06_01_shop.html">Shop</a></li>
+                    <li><a href="06_02_single_product.html">Single Product</a></li>
+                    <li><a href="06_03_cart.html">Cart</a></li>
+                    <li><a href="06_04_checkout.html">Checkout</a></li>
+                </ul>
+            </li>
+            <li class="has-sub"><a href='#'>Element</a>
+                <ul>
+                    <li class="has-sub"><a href="#">Short Code 1</a>
+                        <ul>
+                            <li><a href="07_01_buttons.html"><i class="fa fa-plus-square" aria-hidden="true"></i>Buttons</a></li>
+                            <li><a href="07_02_icons_box.html"><i class="fa fa-cube" aria-hidden="true"></i>Icon Box</a></li>
+                            <li><a href="07_03_progress.html"><i class="fa fa-tasks" aria-hidden="true"></i>Process Bar</a></li>
+                            <li><a href="07_04_tabs.html"><i class="fa fa-columns" aria-hidden="true"></i>Tabs</a></li>
+                        </ul>
+                    </li>
+                    <li class="has-sub"><a href="#">Short Code 2</a>
+                        <ul>
+                            <li><a href="07_05_accordion.html"><i class="fa fa-list" aria-hidden="true"></i>Accordion</a></li>
+                            <li><a href="07_06_counter.html"><i class="fa fa-tachometer" aria-hidden="true"></i>Counter</a></li>
+                            <li><a href="07_07_testimonials.html"><i class="fa fa-comments-o" aria-hidden="true"></i>Testimonials</a></li>
+                            <li><a href="07_08_typography.html"><i class="fa fa-text-width" aria-hidden="true"></i>Typography</a></li>
+                        </ul>
+                    </li>
+                    <li class="has-sub"><a href="#">Short Code 3</a>
+                        <ul>
+                            <li><a href="07_09_partner.html"><i class="fa fa-handshake-o" aria-hidden="true"></i> Partner</a></li>
+                            <li><a href="07_10_team.html"><i class="fa fa-users" aria-hidden="true"></i> Team</a></li>
+                            <li><a href="07_11_item_list.html"><i class="fa fa-list-ol" aria-hidden="true"></i> Item List</a></li>
+                            <li><a href="07_12_divider.html"><i class="fa fa-chain-broken" aria-hidden="true"></i>Dividers</a></li>
+                        </ul>
+                    </li>
+                    <li class="has-sub"><a href="#">Short Code 4</a>
+                        <ul>
+                            <li><a href="07_13_columns.html"><i class="fa fa-bar-chart" aria-hidden="true"></i> Columns</a></li>
+                            <li><a href="07_14_pricing_table.html"><i class="fa fa-address-card-o" aria-hidden="true"></i> Pricing table</a></li>
+                            <li><a href="#"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 404 Pages</a></li>
+                            <li><a href="#"><i class="fa fa-repeat" aria-hidden="true"></i> Comming soon</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="02_04_contact.html">Contact</a></li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+</nav>
+<!-- End mobile menu -->
+{{mo}}
+
+<div class="uni-home-1">
+
+    {{Content}}
+
+</div>
 
 
 
@@ -26,12 +142,41 @@
 
 
 
+<script src="plugin/jquery/jquery-2.0.2.min.js"></script>
+<script src="plugin/jquery-ui/jquery-ui.min.js"></script>
+<script src="plugin/bootstrap/js/bootstrap.js"></script>
+<script src="plugin/process-bar/tox-progress.js"></script>
+<script src="plugin/waypoint/jquery.waypoints.min.js"></script>
+<script src="plugin/counterup/jquery.counterup.min.js"></script>
+<script src="plugin/owl-carouse/owl.carousel.min.js"></script>
+<script src="plugin/jquery-ui/jquery-ui.min.js"></script>
+<script src="plugin/mediaelement/mediaelement-and-player.js"></script>
+<script src="plugin/masonry/masonry.pkgd.min.js"></script>
+<script src="plugin/datetimepicker/moment.min.js"></script>
+<script src="plugin/datetimepicker/bootstrap-datepicker.min.js"></script>
+<script src="plugin/datetimepicker/bootstrap-datepicker.tr.min.js"></script>
+<script src="plugin/datetimepicker/bootstrap-datetimepicker.js"></script>
+<script src="plugin/datetimepicker/bootstrap-datetimepicker.fr.js"></script>
 
+<script src="plugin/lightgallery/picturefill.min.js"></script>
+<script src="plugin/lightgallery/lightgallery.js"></script>
+<script src="plugin/lightgallery/lg-pager.js"></script>
+<script src="plugin/lightgallery/lg-autoplay.js"></script>
+<script src="plugin/lightgallery/lg-fullscreen.js"></script>
+<script src="plugin/lightgallery/lg-zoom.js"></script>
+<script src="plugin/lightgallery/lg-hash.js"></script>
+<script src="plugin/lightgallery/lg-share.js"></script>
+<script src="plugin/sticky/jquery.sticky.js"></script>
 
-
-
-  <!-- MDB -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 
+<!--   [XR&CO'2014], Tue, 22 Oct 2019 11:45:22 GMT -->
 </html>
+
+
+
+
+
+
+{{Content}}
